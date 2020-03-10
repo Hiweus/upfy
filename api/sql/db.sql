784b490c37a524058a709363ac0a77ed2a7c99b6
Hiweus <hiweus@leocadio.com>
2020-03-10 10:20:04 -0300

    Work: appended dump sql to project

--- api/sql/db.sql
+++ api/sql/db.sql
@@ -1,42 +1,138 @@
--- Adminer 4.7.6 MySQL dump
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 10, 2020 at 10:17 AM
+-- Server version: 10.1.44-MariaDB-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.3
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-DROP DATABASE IF EXISTS `upfy`;
-CREATE DATABASE `upfy` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `upfy`
+--
+CREATE DATABASE IF NOT EXISTS `upfy` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `upfy`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth`
+--
 
 DROP TABLE IF EXISTS `auth`;
 CREATE TABLE `auth` (
   `token` char(64) NOT NULL,
   `fk_user` int(11) NOT NULL,
   `creation_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_visit` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`token`)
+  `last_visit` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `auth` (`token`, `fk_user`, `creation_time`, `last_visit`) VALUES
-('5026d80099cbf1871065e5a331380f025a392698f52981c9dd074f74d28fde19',	14,	'2020-03-09 19:03:24',	'2020-03-09 19:03:24'),
-('688f4eafe4c8aa55e498902f2593c1e64efdb84f4b3e541d65e51c049d3493aa',	14,	'2020-03-09 19:02:43',	'2020-03-09 19:02:43'),
-('7c82c02a2218cfae5dc6d2d9c6d5efd995596158abaffa0950e76900a5d4e998',	14,	'2020-03-09 19:04:05',	'2020-03-09 19:20:13'),
-('9c9df8e1ce3ed5abd48f8887c2f1ca2c77a0e4c9fa37e03c823fce1ec7403d1c',	14,	'2020-03-09 19:03:25',	'2020-03-09 19:03:25'),
-('asd',	14,	'2020-03-09 18:32:15',	'2020-03-09 18:56:08');
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
+CREATE TABLE `file` (
+  `token` char(40) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `content` longblob NOT NULL,
+  `creation_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `owner_file`
+--
+
+DROP TABLE IF EXISTS `owner_file`;
+CREATE TABLE `owner_file` (
+  `fk_token` char(40) NOT NULL,
+  `fk_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `pass` char(64) NOT NULL,
   `creation_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `user` (`id`, `name`, `pass`, `creation_time`, `update_time`) VALUES
-(13,	'andreus',	'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',	'2020-03-08 19:55:26',	'2020-03-09 19:20:13'),
-(14,	'andre',	'cbeb0102202435f4c80d0ce7c5fb54070a2ab0a7e98f0fc57efd4005561a20c0',	'2020-03-08 19:55:47',	'2020-03-08 19:55:47');
+--
+-- Dumping data for table `user`
+--
 
--- 2020-03-10 00:56:11
+INSERT INTO `user` (`id`, `name`, `pass`, `creation_time`, `update_time`) VALUES
+(13, 'andre', 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', '2020-03-08 19:55:26', '2020-03-10 07:27:51'),
+(14, 'andre', 'cbeb0102202435f4c80d0ce7c5fb54070a2ab0a7e98f0fc57efd4005561a20c0', '2020-03-08 19:55:47', '2020-03-08 19:55:47');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `auth`
+--
+ALTER TABLE `auth`
+  ADD PRIMARY KEY (`token`);
+
+--
+-- Indexes for table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`token`);
+
+--
+-- Indexes for table `owner_file`
+--
+ALTER TABLE `owner_file`
+  ADD PRIMARY KEY (`fk_token`,`fk_user`),
+  ADD KEY `fk_user` (`fk_user`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `owner_file`
+--
+ALTER TABLE `owner_file`
+  ADD CONSTRAINT `owner_file_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `user` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
